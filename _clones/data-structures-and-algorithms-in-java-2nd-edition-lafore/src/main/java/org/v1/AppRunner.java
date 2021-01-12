6208b18f7b0a90a91f90b80a5942e138e67d5152
hsmak <husain.alkhamis@gmail.com>
2021-01-12 14:40:48 -0600

    committing changes..

---
+++ _clones/data-structures-and-algorithms-in-java-2nd-edition-lafore/src/main/java/org/v1/AppRunner.java
@@ -0,0 +1,7 @@
+package org.v1;
+
+public class AppRunner {
+    public static void main(String[] args) {
+        System.out.println("running");
+    }
+}
---
+++ _clones/data-structures-and-algorithms-in-java-6th-edition-goodrich-tamassia/src/main/java/org/v2/AppRunner.java
@@ -0,0 +1,7 @@
+package org.v2;
+
+public class AppRunner {
+    public static void main(String[] args) {
+        System.out.println("running");
+    }
+}
